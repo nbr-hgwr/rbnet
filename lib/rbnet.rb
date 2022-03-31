9c0184ff389f24193b368d769f5e4ce7de28b122
nbr-hgwr <mp7.fal@gmail.com>
2022-04-01 00:15:37 +0900

    delete debug files

--- lib/rbnet.rb
+++ lib/rbnet.rb
@@ -3,12 +3,8 @@
 require_relative "rbnet/version"
 require_relative "rbnet/bridge"
 require_relative "rbnet/cli"
-require_relative "rbnet/executor"
 require_relative "rbnet/interface"
-require_relative "rbnet/analyzer"
-require_relative "rbnet/resource/packet_info"
 require_relative "rbnet/resource/type"
-require_relative "rbnet/resource/macaddr"
 
 module Rbnet
   class Error < StandardError; end
